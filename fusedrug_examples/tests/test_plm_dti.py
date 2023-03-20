1143d06aa5bea80c9fb6fe9bfe2be45b29d38e89
Daniel Shats Daniel.Shats1@ibm.com <shatz@cccxc422.pok.ibm.com>
2023-03-20 09:05:12 -0400

    override num_epochs

--- fusedrug_examples/tests/test_plm_dti.py
+++ fusedrug_examples/tests/test_plm_dti.py
@@ -7,6 +7,7 @@ class PLMDTITestCase(unittest.TestCase):
     def test_main(self):
         config_path = Path(__file__, "../../interaction/drug_target/affinity_prediction/PLM_DTI/configs/train_config.yaml")
         cfg = OmegaConf.load(config_path)
+        cfg["trainer"]["epochs"] = 2
         main(cfg)
 
 if __name__ == "__main__":
