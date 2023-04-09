d1914dbe9b15a5f4880d97167c57968274392ec6
VADIM RATNER VADIMRA@il.ibm.com <vadimra@cccxc563.pok.ibm.com>
2023-04-09 08:12:25 -0400

    moltitokenizer implementation

--- fusedrug_examples/interaction/drug_target/affinity_prediction/t5_mol/tokenizer/test_multi_tokenizer.py
+++ fusedrug_examples/interaction/drug_target/affinity_prediction/t5_mol/tokenizer/test_multi_tokenizer.py
@@ -4,6 +4,24 @@ from multi_tokenizer import ModularTokenizer
 import os
 from typing import Dict, Optional
 
+from tokenizer.special_tokens import (
+    get_special_tokens,
+)
+from tokenizers import (
+    models,
+    trainers,
+    Tokenizer,
+)
+import pandas as pd
+
+
+TITAN_AA_PATH = (
+    "/dccstor/fmm/users/vadimra/dev/data/TITAN/08-02-2023/public/epitopes.csv"
+)
+TITAN_SMILES_PATH = (
+    "/dccstor/fmm/users/vadimra/dev/data/TITAN/08-02-2023/public/epitopes.smi"
+)
+
 
 def test_tokenizer(
     t_inst: ModularTokenizer, cfg_raw: Dict, mode: Optional[str] = ""
@@ -46,12 +64,49 @@ def test_tokenizer(
     a = 1
 
 
+def create_base_AA_tokenizer(cfg_raw: Dict):
+    def get_training_corpus(dataset):
+        for i in range(0, len(dataset), 1000):
+            yield dataset[i : i + 1000]
+
+    AA_vocab_data = pd.read_csv(
+        TITAN_AA_PATH, sep="\t", header=None, names=["repr", "ID"]
+    )
+
+    # Tokenizer example taken from https://huggingface.co/course/chapter6/8?fw=pt
+
+    ############################## unwrapped AA tokenizer: AAs are treated as letters
+    unwrapped_AA_tokenizer = Tokenizer(models.BPE())
+    special_tokens = get_special_tokens(subset=["special", "task"])
+    trainer_AA = trainers.BpeTrainer(vocab_size=100, special_tokens=special_tokens)
+    unwrapped_AA_vocab = list(AA_vocab_data["repr"])
+    unwrapped_AA_tokenizer.train_from_iterator(
+        get_training_corpus(dataset=unwrapped_AA_vocab), trainer=trainer_AA
+    )
+    if not os.path.exists(
+        os.path.dirname(
+            cfg_raw["data"]["tokenizer"]["tokenizers_info"]["AA"]["json_path"]
+        )
+    ):
+        os.makedirs(
+            os.path.dirname(
+                cfg_raw["data"]["tokenizer"]["tokenizers_info"]["AA"]["json_path"]
+            )
+        )
+    unwrapped_AA_tokenizer.save(
+        path=cfg_raw["data"]["tokenizer"]["tokenizers_info"]["AA"]["json_path"]
+    )
+    print("Fin")
+
+
 @hydra.main(config_path="../configs", config_name="train_config", version_base=None)
 def main(cfg: DictConfig) -> None:
     print(str(cfg))
 
     cfg = hydra.utils.instantiate(cfg)
     cfg_raw = OmegaConf.to_object(cfg)
+
+    create_base_AA_tokenizer(cfg_raw=cfg_raw)
 
     t_mult = ModularTokenizer(**cfg_raw["data"]["tokenizer"])
     t_mult.save_jsons(tokenizers_info=cfg_raw["data"]["tokenizer"]["tokenizers_info"])
