0d7cd368dbe7d5d64a3c01d7df4261410fec3a91
YoelShoshan <yoels@il.ibm.com>
2023-04-24 05:22:02 -0400

    ...

--- fusedrug/utils/misc.py
+++ fusedrug/utils/misc.py
@@ -15,5 +15,12 @@ def worker_init_fn(worker_id: int) -> None:
     # to ensure different seed on each worker
     worker_seed = (torch.initial_seed() + os.getpid() + worker_id) % (2**32)
     print(f"worker_init_fn - seed={worker_seed}")
-    np.random.seed(worker_seed)
-    random.seed(worker_seed)
+    set_rng_seed(worker_seed)
+    # add torch random seed selection as well?
+
+
+def set_rng_seed(seed:int) -> None:
+    print(f"set_rng_seed: setting python random, numpy and torch rng seeds to {seed}")
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
