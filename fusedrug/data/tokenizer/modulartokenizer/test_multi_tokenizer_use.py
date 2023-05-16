c7edd9023c84c3e5c945924d03a328b9eb9a303b
VADIM RATNER VADIMRA@il.ibm.com <vadimra@cccxc451.pok.ibm.com>
2023-05-16 05:26:57 -0400

    ModularTokenizer path-only load and save

--- fusedrug/data/tokenizer/modulartokenizer/test_multi_tokenizer_use.py
+++ fusedrug/data/tokenizer/modulartokenizer/test_multi_tokenizer_use.py
@@ -2,10 +2,10 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 from multi_tokenizer import ModularTokenizer
 from typing import Dict, Any
-from .test_multi_tokenizer_creation import test_tokenizer
+from test_multi_tokenizer_creation import test_tokenizer
 
 
-@hydra.main(config_path="./configs", config_name="train_config", version_base=None)
+@hydra.main(config_path="./configs", config_name="tokenizer_config", version_base=None)
 def main(cfg: DictConfig) -> None:
     print(str(cfg))
 
@@ -16,6 +16,10 @@ def main(cfg: DictConfig) -> None:
     t_mult_loaded = ModularTokenizer.load_from_jsons(tokenizers_info=cfg_raw["data"]["tokenizer"]["tokenizers_info"])
 
     test_tokenizer(t_mult_loaded, cfg_raw=cfg_raw, mode="loaded")
+
+    t_mult_loaded_path = ModularTokenizer.load(path=cfg_raw["data"]["tokenizer"]["out_path"])
+
+    test_tokenizer(t_mult_loaded_path, cfg_raw=cfg_raw, mode="loaded_path")
 
     print("Fin")
 
