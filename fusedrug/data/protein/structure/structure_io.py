5d963844d1851f8b0a15564296b4e4543dd00f80
YoelShoshan <yoels@il.ibm.com>
2023-05-30 10:05:06 -0400

    ...

--- fusedrug/data/protein/structure/structure_io.py
+++ fusedrug/data/protein/structure/structure_io.py
@@ -64,7 +64,7 @@ def save_structure_file(
     b_factors: Optional[Dict[str, torch.Tensor]] = None,
     reference_cif_filename: Optional[str] = None,
     mask: Optional[List] = None,
-) -> None:
+) -> List[str]:
     """
     A helper function allowing to save single or multi chain structure into pdb and/or mmcif format.
 
@@ -80,6 +80,9 @@ def save_structure_file(
         b_factors -
         reference_cif_filename:Optional[str] - for mmCIF outputs you must provide an mmCIF reference file (you can use the ground truth one)
         mask:Optional[List] - a mask describing which residues to store
+
+    Returns:
+        A list with paths for all saved files
     """
     assert save_pdb or save_cif
     assert len(chain_to_atom14) > 0
@@ -92,6 +95,8 @@ def save_structure_file(
         sorted_chain_ids = sorted(list(chain_to_aa_str_seq.keys()))
 
     assert (chain_to_aa_str_seq is not None) or (chain_to_aa_index_seq is not None)
+
+    all_saved_files = []
 
     if save_cif:
         if len(chain_to_aa_str_seq) > 1:
@@ -107,6 +112,8 @@ def save_structure_file(
                     chain_to_atom14=chain_to_atom14,
                     output_mmcif_filename=out_multimer_cif_filename,
                 )
+
+                all_saved_files.append(out_multimer_cif_filename)
             else:
                 print(
                     f'not writing multimer file because "reference_cif_filename" was not provided for {reference_cif_filename}'
@@ -134,6 +141,8 @@ def save_structure_file(
                 model=0,
             )
 
+            all_saved_files.append(out_pdb)
+
         if save_cif:
             out_cif = output_filename_extensionless + "_chain_" + chain_id + ".cif"
             if reference_cif_filename is not None:
@@ -145,10 +154,13 @@ def save_structure_file(
                     chain_to_atom14={chain_id: chain_to_atom14[chain_id]},
                     output_mmcif_filename=out_cif,
                 )
+
+                all_saved_files.append(out_cif)
             else:
                 print(
                     f'not writing chain cif file because no "reference_cif_filename" was provided for {reference_cif_filename}'
                 )
+    return all_saved_files
 
 
 def get_chain_native_features(
