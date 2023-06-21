2885bf0eb483a3d62d34254c781da503ca6d4cfe
Sagi Polaczek <56922146+SagiPolaczek@users.noreply.github.com>
2023-06-21 13:31:41 +0300

    Support more args for `mmseqs` clustering (#48)
    # ✅ 
    
    Added additional arguments for `mmseqs` clustering + fixing typos
    
    ---------
    
    Co-authored-by: Sagi Polaczek <sagi.polaczek@ibm.com>

--- fusedrug/utils/cluster/cluster_using_mmseqs.py
+++ fusedrug/utils/cluster/cluster_using_mmseqs.py
@@ -4,7 +4,7 @@ import hashlib
 import subprocess
 from fuse.utils.file_io import save_text_file_safe, read_text_file
 import os
-from typing import Dict
+from typing import Dict, Optional
 from os.path import join
 import yaml
 import pandas as pd
@@ -15,7 +15,7 @@ def cached_cluster(output_dir: str, force_rebuild: bool = False, **kwargs: dict)
     Uses mmseqs to:
 
     1. Remove 100% sequence identity duplicates
-    2. Cluster the remaining unique sequences into multiple clusters (e.g. with 70% sequence similary threshold )
+    2. Cluster the remaining unique sequences into multiple clusters (e.g. with 70% sequence similarity threshold )
         This is useful for multiple purposes:
         a. Creating cross validation/test splits that evaluate and demonstrate generalizability
         b. Balanced sampling during training, sampling with inverse proportion to cluster size. (Similar to Class Balancing)
@@ -30,14 +30,18 @@ def cached_cluster(output_dir: str, force_rebuild: bool = False, **kwargs: dict)
         force_rebuild: rebuilds the data even if the hash indicates that it was already cached.
         input_fasta_filename: a fasta with an entry per molecular entity
         output_dir: where the output will be generated
-        cluster_min_seqeunce_identity: the minimal sequence identity for member within a cluster
-        threads: number of threads (for multithreading)
+        cluster_min_sequence_identity: the minimal sequence identity for member within a cluster
+        threads: number of threads (for multithreading). if None -> by default it will use as many as the system has
         cluster_method: any of 'cluster', 'linclust':
             'cluster' is the "vanilla" one
             'linclust' is faster (claims linear runtime) but less accurate. Might be suitable for massive data.
                 NOTE: I've compared cluster and linclust results for the deduplication phase, and results aren't identical, which means it probably misses few identical cases.
         deduplicate: if False, deduplication step will be skipped and clustering will be done directly on the input
+        kmer_per_seq: Sets the number of k-mers selected per sequence in a "linclust" cluster_method. More k-mers per sequences results in a higher sensitivity.
+        split_memory_limit: (optional), limit the memory usage. should be max 70% of system's available RAM
+        override_workspace: when set to 'True', will automatically override the mmseqs workspace.
 
+    For more information visit here -> https://mmseqs.com/latest/userguide.pdf
     Note - this function wraps cluster_impl() to allow caching
     """
     os.makedirs(output_dir, exist_ok=True)
@@ -87,15 +91,17 @@ def cluster(
     *,
     input_fasta_filename: str,
     output_dir: str,
-    cluster_min_seqeunce_identity: float = 0.4,
-    threads: int = 30,
+    cluster_min_sequence_identity: float = 0.4,
+    threads: Optional[int] = None,
     cluster_method: str = "cluster",
     deduplicate: bool = True,
+    override_workspace: bool = False,
+    kmer_per_seq: Optional[int] = None,
+    split_memory_limit: Optional[str] = None,
 ) -> Dict[str, str]:
     """
     see cached_cluster() doc
     """
-
     which_mmseqs = shutil.which("mmseqs")
 
     if which_mmseqs is None:
@@ -111,14 +117,17 @@ def cluster(
             cmd = f"gunzip {input_fasta_filename}.gz"
             _run_system_cmd(cmd)
 
-    os.makedirs(join(output_dir, "mmseqs_workspace"))
+    # Create workspace (supports overriding)
+    workspace_dir = join(output_dir, "mmseqs_workspace")
+    if override_workspace and os.path.exists(workspace_dir):
+        shutil.rmtree(workspace_dir)
 
+    os.makedirs(workspace_dir)
     mmseqs_db_path = join(output_dir, "mmseqs_workspace", "mmseqs_DB")
 
     print("cluster_method=", cluster_method)
 
     ans = {}
-
     ########### Major step A - remove all redundancies
     if deduplicate:
 
@@ -134,10 +143,11 @@ def cluster(
         print(
             r"A.2 - clustering with 100% identity to remove duplicates. The generated DB does not contain (directly) the sequences data, it only maps clusters centers to members."
         )
-        cmd = f"mmseqs {cluster_method} {mmseqs_db_path} {mmseqs_cluster_full_identity} {mmseqs_tmp_for_clustering} -c 1.0 --min-seq-id 1.0 --threads {threads}"
+        cmd = f"mmseqs {cluster_method} {mmseqs_db_path} {mmseqs_cluster_full_identity} {mmseqs_tmp_for_clustering} -c 1.0"
+        cmd = _handle_mmseqs_cli_arguments(cmd, threads=threads, split_memory_limit=split_memory_limit, min_seq_id=1.0)
         _run_system_cmd(cmd)
 
-        mmseqs_only_representatives = join(output_dir, "mmseqs_workspace", "mmseqs_DB_full_identity_representitives")
+        mmseqs_only_representatives = join(output_dir, "mmseqs_workspace", "mmseqs_DB_full_identity_representatives")
         print(r"A.3 - keeping only cluster centers")
         cmd = f"mmseqs createsubdb {mmseqs_cluster_full_identity} {mmseqs_db_path}  {mmseqs_only_representatives}"
         _run_system_cmd(cmd)
@@ -152,7 +162,7 @@ def cluster(
     ########### Major step B - create clusters
 
     # description on how to read the cluster files format: https://mmseqs.com/latest/userguide.pdf - search for "Internal cluster format",
-    # also describes how to convert it to TSV for convinience
+    # also describes how to convert it to TSV for convenience
 
     print("B.1 - creating mmseqs DB for our unique DB")
     step_B_initial_db = join(output_dir, "mmseqs_workspace", "step_B_initial_DB")
@@ -169,11 +179,18 @@ def cluster(
     )
     mmseqs_tmp_2_for_clustering = join(output_dir, "mmseqs_workspace", "mmseqs_DB_tmp_2")
     clustered_db = join(output_dir, "mmseqs_workspace", "mmseqs_DB_clustered")
-    cmd = f"mmseqs {cluster_method} {step_B_initial_db} {clustered_db} {mmseqs_tmp_2_for_clustering} --cov-mode 0 -c 1.0 --min-seq-id {cluster_min_seqeunce_identity} --threads {threads}"
+    cmd = f"mmseqs {cluster_method} {step_B_initial_db} {clustered_db} {mmseqs_tmp_2_for_clustering} -c 1.0"
+    cmd = _handle_mmseqs_cli_arguments(
+        cmd,
+        threads=threads,
+        kmer_per_seq=kmer_per_seq,
+        split_memory_limit=split_memory_limit,
+        min_seq_id=cluster_min_sequence_identity,
+    )
     _run_system_cmd(cmd)
 
     print(
-        "B.3 - generate cluster TSV for convinience"
+        "B.3 - generate cluster TSV for convenience"
     )  # for massive datasets, we might skip this and use mmseqs output format directly (possibly worth checking if there's already a python lib that handles this)
     clustered_tsv = join(output_dir, "clustered.tsv")
     cmd = f"mmseqs createtsv {step_B_initial_db} {step_B_initial_db} {clustered_db} {clustered_tsv}"
@@ -188,7 +205,7 @@ def cluster(
 
     ####
     final_clusters_mmseqs_only_representatives = join(
-        output_dir, "mmseqs_workspace", "mmseqs_DB_final_clusters_representitives"
+        output_dir, "mmseqs_workspace", "mmseqs_DB_final_clusters_representatives"
     )
     print(r"B.4 - create a sub DB only with the clusters centers (representatives)")
     cmd = f"mmseqs createsubdb {clustered_db} {step_B_initial_db}  {final_clusters_mmseqs_only_representatives}"
@@ -204,6 +221,7 @@ def cluster(
     print("--------------------------------------")
     print("Final generated key files summary:")
     print("--------------------------------------")
+
     if deduplicate:
         print(f"a deduplicated FASTA file: {mmseqs_only_unique_sequences_representatives_fasta}")
         ans["deduplicated_fasta"] = mmseqs_only_unique_sequences_representatives_fasta
@@ -222,14 +240,31 @@ def cluster(
 cluster_impl = cluster  # for backward compatibility
 
 
-def _run_system_cmd(cmd: str) -> None:
-    print("about to run: ", cmd)
-    res = subprocess.run(cmd, shell=True, check=False, capture_output=True)
-    if len(res.stdout) > 0:
+def _run_system_cmd(cmd: str, capture_output: bool = True) -> None:
+    print(f"about to run: {cmd}")
+    res = subprocess.run(cmd, shell=True, check=False, capture_output=capture_output)
+    if res.stdout and len(res.stdout) > 0:
         print("stdout=")
         print(res.stdout.decode())
-    if len(res.stderr) > 0:
+    if res.stderr and len(res.stderr) > 0:
         print("stderr=")
         print(res.stderr.decode())
     if res.returncode != 0:
         raise Exception(f"ERROR: failed when trying to run {cmd}, got return val={res.returncode}")
+
+
+def _handle_mmseqs_cli_arguments(
+    cmd: str,
+    threads: Optional[int] = None,
+    kmer_per_seq: Optional[int] = None,
+    split_memory_limit: Optional[str] = None,
+    min_seq_id: Optional[float] = None,
+) -> str:
+    """
+    Handles optional command line arguments mmseqs calls.
+    """
+    cmd = f"{cmd} --threads {threads}" if threads else cmd
+    cmd = f"{cmd} --kmer-per-seq {kmer_per_seq}" if kmer_per_seq else cmd
+    cmd = f"{cmd} --split-memory-limit {split_memory_limit}" if split_memory_limit else cmd
+    cmd = f"{cmd} --min-seq-id {min_seq_id}"
+    return cmd
