3f34a824b8c8bc6ed7f49c9cb402eb658bc6ceba
YoelShoshan <yoels@il.ibm.com>
2023-10-06 02:29:46 -0400

    ...

--- fusedrug/data/protein/structure/sabdab.py
+++ fusedrug/data/protein/structure/sabdab.py
@@ -20,3 +20,35 @@ def load_sabdab_dataframe(path: Optional[str] = None) -> pd.DataFrame:
         path = os.path.join(os.environ["SABDAB_DIR"], "sabdab_summary_all.tsv")
     df = pd.read_csv(path, sep="\t")
     return df
+
+
+class SAbDAb:
+    def __init__(self, main_dataframe_path: str = None):
+        self.df = load_sabdab_dataframe()
+
+    def get_entry(self, pdb_id: str, heavy_chain_id: Optional[str] = None) -> pd.Series:
+        if heavy_chain_id is not None:
+            found = self.df.loc[
+                (self.df.pdb == pdb_id) & (self.df.Hchain == heavy_chain_id)
+            ]
+        else:
+            found = self.df.loc[self.df.pdb == pdb_id]
+
+        if found.shape[0] == 0:
+            raise Exception(
+                f"could not find an entry for pdb_id={pdb_id} heavy_chain_id={heavy_chain_id}"
+            )
+        elif found.shape[0] > 1:
+            raise Exception(
+                f"found multiple entries for pdb_id={pdb_id} heavy_chain_id={heavy_chain_id}"
+            )
+
+        found = found.iloc[0]
+
+        return found
+
+
+if __name__ == "__main__":
+    inst = SAbDAb()
+    inst.get_entry(pdb_id="7vux", heavy_chain_id="H")
+    inst.get_entry(pdb_id="7vux")
