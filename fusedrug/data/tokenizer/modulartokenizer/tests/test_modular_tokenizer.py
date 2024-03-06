fe7395ebb885434346f7a9d0c3de01d6476ffa87
Matan Ninio <matann@il.ibm.com>
2024-03-06 10:15:40 +0200

    Jenkins DEBUG ONLY - added prints

--- fusedrug/data/tokenizer/modulartokenizer/tests/test_modular_tokenizer.py
+++ fusedrug/data/tokenizer/modulartokenizer/tests/test_modular_tokenizer.py
@@ -20,6 +20,7 @@ CONFIG_NAME = "tokenizer_config"
 
 class ConfigHolder:
     def __init__(self, cfg: DictConfig = None) -> None:
+        print(f' L23 {os.environ.get("MY_GIT_REPOS")}')
         if cfg is None:
             self._setup_test_env()
             with hydra.initialize_config_dir(CONFIG_PATH):
@@ -31,8 +32,9 @@ class ConfigHolder:
 
     def _setup_test_env(self) -> None:
         REPO_ROOT = "fuse-drug"
-        if "MY_GIT_REPO" not in os.environ:
+        if "MY_GIT_REPOS" not in os.environ:
             for i in range(len(Path(__file__).parents)):
+                print(f" L37 dir[{i}] =  {Path(__file__).parents[i].name}")
                 if Path(__file__).parents[i].name == REPO_ROOT:
                     os.environ["MY_GIT_REPOS"] = str(Path(__file__).parents[i + 1])
                     break
