f1be0cf40383dd9c02a54f084ae08eb09a75f942
Moshe Raboh Moshiko.Raboh@ibm.com <moshiko@cccxl010.pok.ibm.com>
2024-09-09 07:28:52 -0400

    ..

--- fusedrug/data/tokenizer/modulartokenizer/special_tokens.py
+++ fusedrug/data/tokenizer/modulartokenizer/special_tokens.py
@@ -12,3 +12,14 @@ def strip_special_wrap(x: str) -> str:
     for spec_wrap in special_token_marker:
         x = x.replace(spec_wrap, "")
     return x
+
+
+# keeping for backward compatibility - it's better not to use it.
+special_tokens = {
+    "unk_token": "UNK",  # Unknown token
+    "pad_token": "PAD",  # Padding token
+    "cls_token": "CLS",  # Classifier token (probably irrelevant in the T5 setting)
+    "sep_token": "SEP",  # Separator token
+    "mask_token": "MASK",  # Mask token
+    "eos_token": "EOS",  # End of Sentence token
+}
