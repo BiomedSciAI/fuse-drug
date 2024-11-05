9183b71e9a55a21e519ce9248a1e4d4b1412bcfb
Moshiko Raboh <86309179+mosheraboh@users.noreply.github.com>
2024-11-05 10:00:55 +0200

    move modular tokenizer to fuse-med-ml (#147)

--- fusedrug/data/tokenizer/modulartokenizer/configs/__init__.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-# The directory containing this file
-CONFIG_DIRPATH = Path(__file__).parent
-
-
-def get_modular_tokenizer_config_dirpath() -> str:
-    return str(CONFIG_DIRPATH.resolve())
--- fusedrug/data/tokenizer/modulartokenizer/pretrained_tokenizers/__init__.py
+++
@@ -1,5 +0,0 @@
-from os.path import dirname
-
-
-def get_dir_path() -> str:
-    return dirname(__file__)
