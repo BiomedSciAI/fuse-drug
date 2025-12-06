4385f9336a43df5b2d9ff044a9abd4d1414b44e6
YoelShoshan <yoelshoshan@gmail.com>
2025-12-06 07:30:00 +0200

    adding utility to convert chain ids in mmcif from author id to pdb id (#160)
    Co-authored-by: yoels <yoels@login-b.zuvela.ibm.com>

--- fusedrug/data/protein/sequence/official_pdb_fasta.py
+++ fusedrug/data/protein/sequence/official_pdb_fasta.py
@@ -1,38 +1,148 @@
 from io import StringIO
 from Bio import SeqIO
 from urllib.request import urlopen
-from typing import Dict
+from typing import Dict, List
 
 
-def get_fasta_from_rcsb(pdb_id: str) -> Dict:  # TODO: consider adding caching
+def get_fasta_from_rcsb(
+    pdb_id: str,
+    author_chain_id: bool = True,
+    verbose: bool = False,
+    return_duplicates_if_found: bool = True,
+) -> Dict:  # TODO: consider adding caching
     """
     Given some pdb_id, (like "7vux"), we will retrieve its fasta file from rcsb database and return it as a dict {chain: sequence}.
+    This version handles symmetry, so if there are multiple identical chains in the same complex, it will return them as separate keys
     """
     fasta_data = (
         urlopen(f"https://www.rcsb.org/fasta/entry/{pdb_id.upper()}")
         .read()
         .decode("utf-8")
     )
-    fasta_file_handle = StringIO(fasta_data)
-    chains_full_seq = SeqIO.to_dict(
-        SeqIO.parse(fasta_file_handle, "fasta"),
-        key_function=lambda rec: _description_to_author_chain_id(rec.description),
-    )
-    chains_full_seq = {k: str(d.seq) for (k, d) in chains_full_seq.items()}
-    return chains_full_seq
+
+    ans = {}
+
+    io_data = StringIO(fasta_data)
+    for record in SeqIO.parse(io_data, "fasta"):
+        print(f"{record.description=}")
+        chains = get_chain_ids(
+            description=record.description, author_chain_id=author_chain_id
+        )
+        print(f"{chains=}")
+
+        for chain in chains:
+            ans[chain] = str(record.seq)
+            if not return_duplicates_if_found:
+                break
+
+    return ans
 
 
-def _description_to_author_chain_id(description: str) -> str:
-    loc = description.find(" ")
-    assert loc >= 0
-    description = description[loc + 1 :]
-    loc = description.find(",")
-    if loc >= 0:
-        description = description[:loc]
+def get_chain_ids(*, description: str, author_chain_id: bool) -> List[str]:
+    """
+    author_chain_id: if True will return the author chain id, if False will return the pdb_chain_id
+    TODO: add support for chain ids that have more than one character
 
-    token = "auth "
-    loc = description.find(token)
-    if loc >= 0:
-        return description[loc + len(token)]
+    args:
+        description: the description line from the fasta file, for example:
+            '9DH2_1|Chains A, C[auth G], E[auth K], H[auth P]|Fab heavy chain|Homo sapiens (9606)'
+        author_chain_id: whether to return the author chain id or the pdb chain id
+    returns:
+        list of chain ids, for example: ['A', 'C', 'E']
+    """
+    loc1 = description.find(" ")
+    assert loc1 >= 0
 
-    return description[0]
+    description = description[loc1 + 1 :]
+
+    loc2 = description.find("|")
+    assert loc2 >= 0
+    description = description[:loc2].strip()
+
+    ans = []
+
+    for part in description.split(","):
+        part = part.strip()
+
+        curr_pdb_chain_id = part[0]
+        curr_author_chain_id = curr_pdb_chain_id
+        if author_chain_id:
+            if " " in part:
+                curr_author_chain_id = part[part.find(" ") + 1]
+
+        if author_chain_id:
+            ans.append(curr_author_chain_id)
+        else:
+            ans.append(curr_pdb_chain_id)
+
+    return ans
+
+
+# examples for reference:
+
+# >6N25_1|Chains A, B, C, D, E|Bestrophin homolog|Gallus gallus (9031)
+#
+
+# seq.description='9DH2_1|Chains A, C[auth G], E[auth K], H[auth P]|Fab heavy chain|Homo sapiens (9606)'
+# chains=['A', 'C', 'E', 'H']
+# seq.description='9DH2_2|Chains B[auth D], D[auth H], F[auth L], I[auth Q]|Fab light chain|Homo sapiens (9606)'
+# chains=['B', 'D', 'F', 'I']
+# seq.description='9DH2_3|Chains G[auth M], J[auth R], K[auth S], L[auth T]|NKG2-D type II integral membrane protein|Homo sapiens (9606)'
+# chains=['G', 'J', 'K', 'L']
+# 9lsh
+# seq.description='9LSH_1|Chains A, B|Citrate/sodium symporter|Klebsiella pneumoniae (573)'
+# chains=['A', 'B']
+# seq.description='9LSH_2|Chain C|Toll-like receptor 3|Homo sapiens (9606)'
+# chains=['C']
+# seq.description='9LSH_3|Chain D|Diabody (CitS VH-TLR3 VL)|synthetic construct (32630)'
+# chains=['D']
+# seq.description='9LSH_4|Chain E|Diabody (TLR3 VH-CitS VL)|synthetic construct (32630)'
+# chains=['E']
+# 9lsj
+# seq.description='9LSJ_1|Chains A, B|Citrate/sodium symporter|Klebsiella pneumoniae (573)'
+# chains=['A', 'B']
+# seq.description='9LSJ_2|Chain C|Toll-like receptor 3|Homo sapiens (9606)'
+# chains=['C']
+# seq.description='9LSJ_3|Chain D|Diabody (CitS VH-TLR3 VL)|synthetic construct (32630)'
+# chains=['D']
+# seq.description='9LSJ_4|Chain E|Diabody (TLR3 VH-CitS VL)|synthetic construct (32630)'
+# chains=['E']
+# 9lsi
+# seq.description='9LSI_1|Chains A, B|Citrate/sodium symporter|Klebsiella pneumoniae (573)'
+# chains=['A', 'B']
+# seq.description='9LSI_2|Chain C|Diabody (CitS VH-TLR3 VL)|synthetic construct (32630)'
+# chains=['C']
+# seq.description='9LSI_3|Chain D|Diabody (TLR3 VH-CitS VL)|synthetic construct (32630)'
+# chains=['D']
+# seq.description='9LSI_4|Chain E|Toll-like receptor 3|Homo sapiens (9606)'
+# chains=['E']
+# 8vqf
+# seq.description='8VQF_1|Chain A|IgE 1J11 Light chain|Homo sapiens (9606)'
+# chains=['A']
+# seq.description='8VQF_2|Chain B|IgE 1J11 Heavy chain|Homo sapiens (9606)'
+# chains=['B']
+# seq.description='8VQF_3|Chain C|Major allergen Can f 1|Canis lupus familiaris (9615)'
+# chains=['C']
+# 9h4r
+# seq.description='9H4R_1|Chains A, D[auth B]|Zona pellucida sperm-binding protein 2|Mus musculus (10090)'
+# chains=['A', 'D']
+# seq.description='9H4R_2|Chains B[auth H], E[auth X]|Heavy chain variable (VH) domain of anti-ZP2 monoclonal antibody IE-3|Rattus norvegicus (10116)'
+# chains=['B', 'E']
+# seq.description='9H4R_3|Chains C[auth L], F[auth Y]|Light chain variable (VL) domain of anti-ZP2 monoclonal antibody IE-3|Rattus norvegicus (10116)'
+# chains=['C', 'F']
+# 9h4s
+# seq.description='9H4S_1|Chains A, D[auth B]|Zona pellucida sperm-binding protein 2|Mus musculus (10090)'
+# chains=['A', 'D']
+# seq.description='9H4S_2|Chains B[auth H], E[auth X]|Heavy chain variable (VH) domain of anti-ZP2 monoclonal antibody IE-3|Rattus norvegicus (10116)'
+# chains=['B', 'E']
+# seq.description='9H4S_3|Chains C[auth L], F[auth Y]|Light chain variable (VL) domain of anti-ZP2 monoclonal antibody IE-3|Rattus norvegicus (10116)'
+# chains=['C', 'F']
+# 8vba
+# seq.description='8VBA_1|Chain A|EtpA|Escherichia coli ETEC H10407 (316401)'
+# chains=['A']
+# seq.description='8VBA_2|Chain B[auth H]|mAb 1C08 Heavy Chain|Mus musculus (10090)'
+# chains=['B']
+# seq.description='8VBA_3|Chain C[auth L]|mAb 1C08 Light Chain|Mus musculus (10090)'
+# chains=['C']
+# 8vbb
+# seq.description='8VBB_1|Chain A|EtpA|Escherichia coli ETEC H10407 (316401)'
---
+++ fusedrug/data/protein/structure/mmcif_author_id_to_pdb_id_convertor.py
@@ -0,0 +1,123 @@
+from fusedrug.data.protein.structure.structure_io import load_protein_structure_features
+from Bio import PDB
+import io
+from typing import Any, Mapping, Sequence, Dict
+
+from tiny_openfold.data.mmcif_parsing import (
+    mmcif_loop_to_list,
+    mmcif_loop_to_dict,
+    _handle_residue_id_duplication,
+    _get_atom_site_list,
+)
+
+
+def get_author_assigned_pdb_id_to_pdb_assigned_pdb_id_convertor(
+    *,
+    mmcif_path: str,
+    pdb_id: str,
+) -> Dict:
+    features, mmcif_object, mmcif2dict = load_protein_structure_features(
+        pdb_id_or_filename=mmcif_path, pdb_id=pdb_id
+    )
+    convertion = mmcif_object.author_assigned_chain_id_to_pdb_assigned_chain_id
+    return convertion
+
+
+def _get_author_assigned_pdb_id_to_pdb_assigned_pdb_id_convertor_helper(
+    *,
+    mmcif_string: str,
+    handle_residue_id_duplication: bool = False,
+    quiet_parsing: bool = True,
+) -> dict:
+    """Entry point, parses an mmcif_string.
+
+    Args:
+      mmcif_string: Contents of an mmCIF file.
+      handle_residue_id_duplication: If True, will handle cases where residue ids are duplicated
+      quiet_parsing: If True, will suppress messages during parsing.
+    Returns:
+      A dictionary mapping author assigned chain ids to pdb assigned chain ids
+    """
+
+    # errors = {}
+    try:
+        parser = PDB.MMCIFParser(QUIET=quiet_parsing)
+        handle = io.StringIO(mmcif_string)
+        full_structure = parser.get_structure("", handle)
+        _ = next(full_structure.get_models())  # get the first structure
+        # Extract the _mmcif_dict from the parser, which contains useful fields not
+        # reflected in the Biopython structure.
+        parsed_info = parser._mmcif_dict  # pylint:disable=protected-access
+
+        # Ensure all values are lists, even if singletons.
+        for key, value in parsed_info.items():
+            if not isinstance(value, list):
+                parsed_info[key] = [value]
+
+        # header = _get_header(parsed_info)
+
+        # Determine the protein chains, and their start numbers according to the
+        # internal mmCIF numbering scheme (likely but not guaranteed to be 1).
+        _ = _get_all_chains(
+            parsed_info=parsed_info,
+            handle_residue_id_duplication=handle_residue_id_duplication,
+        )
+
+        mmcif_to_author_chain_id = {}
+        for atom in _get_atom_site_list(parsed_info):
+            if atom.model_num != "1":
+                # We only process the first model at the moment.
+                continue
+
+            mmcif_to_author_chain_id[atom.mmcif_chain_id] = atom.author_chain_id
+
+    except Exception as e:
+        print(e)
+        # return None
+        raise
+
+    # flip the direction, getting "author assigned pdb id" to "pdb assigned pdb id" dictionary
+
+    author_assigned_to_pdb_assigned = {
+        c2: c1 for (c1, c2) in mmcif_to_author_chain_id.items()
+    }
+
+    return author_assigned_to_pdb_assigned
+
+
+def _get_all_chains(
+    *,
+    parsed_info: Mapping[str, Any],
+    handle_residue_id_duplication: bool = False,
+) -> Sequence[str]:
+    # Get polymer information for each entity in the structure.
+    entity_poly_seqs = mmcif_loop_to_list("_entity_poly_seq.", parsed_info)
+
+    if handle_residue_id_duplication:
+        entity_poly_seqs = _handle_residue_id_duplication(
+            entity_id="_entity_poly_seq.entity_id",
+            residue_num="_entity_poly_seq.num",
+            data=entity_poly_seqs,
+            logic="keep_last",
+        )
+
+    # Get chemical compositions. Will allow us to identify which of these polymers
+    # are proteins.
+    _ = mmcif_loop_to_dict("_chem_comp.", "_chem_comp.id", parsed_info)
+
+    # Get chains information for each entity. Necessary so that we can return a
+    # dict keyed on chain id rather than entity.
+    struct_asyms = mmcif_loop_to_list("_struct_asym.", parsed_info)
+
+    # entity_to_mmcif_chains = collections.defaultdict(list)
+    # for struct_asym in struct_asyms:
+    #     chain_id = struct_asym["_struct_asym.id"]
+    #     entity_id = struct_asym["_struct_asym.entity_id"]
+    #     entity_to_mmcif_chains[entity_id].append(chain_id)
+
+    all_seen_chain_ids = []
+    for struct_asym in struct_asyms:
+        chain_id = struct_asym["_struct_asym.id"]
+        all_seen_chain_ids.append(chain_id)
+
+    return all_seen_chain_ids
